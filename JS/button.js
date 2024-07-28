68a0866b02020a3015c07cee5924df77f8533bf3
nkikn <nkikn21@freeuni.edu.ge>
2024-07-28 20:13:26 +0400

    UPDATE: add minor changes

---
+++ JS/button.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menuContent = document.getElementById("menu-content");
+    if (menuContent.style.display === "none") {
+        menuContent.style.display = "block";
+    } else {
+        menuContent.style.display = "none";
+    }
+}
